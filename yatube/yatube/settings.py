f3f7e9dc64e4d726a480c886e8be18f1a4fdb3b2
Сергей Белоусов <ss.belousov@yandex.ru>
2023-03-01 20:45:37 +0300

    Готовая версия

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ')iyjf_z%sssj)c*dr)v_8673*y#ofe59v05*rg9402fg=n+zg&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',
